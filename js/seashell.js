55f590db1c93d10c7ab3d0839645c6ac8d0be3b7
cheeriocheng <cheeriocheng@gmail.com>
2016-11-14 15:28:11 -0800

    working visually but with overlaping vertex

--- js/seashell.js
+++ js/seashell.js
@@ -72,70 +72,123 @@ function init() {
     
     ss = new Seashell();
     //spine of seashell 
-    // var spiral = ss.spiral; 
+    var spiral = ss.spiral; 
     
    
      //DRAW IN DOTS ----
-    var sphere; 
-    var pos; 
-    var radius = 0.5; //— sphere radius. Default is 50.
-    var widthSegments = 10; //— number of horizontal segments
-    var heightSegments = 10; 
+   //  var sphere; 
+   //  var pos; 
+   //  var radius = 0.5; //— sphere radius. Default is 50.
+   //  var widthSegments = 10; //— number of horizontal segments
+   //  var heightSegments = 10; 
 
-    var material = new THREE.MeshPhongMaterial({side: THREE.DoubleSide,  color: 0xFFFFFF, shading: THREE.SmoothShading}); //FlatShading:SmoothShading
-   console.log("spiral length" , ss.spiral.length);
-   console.log("circle length" , ss._shell[80].length);
+   //  var material = new THREE.MeshPhongMaterial({side: THREE.DoubleSide,  color: 0xFFFFFF, shading: THREE.SmoothShading}); //FlatShading:SmoothShading
+   // console.log("spiral length" , ss.spiral.length);
+   // console.log("circle length" , ss._shell[80].length);
    
-    for (var i = 80 ; i< ss.spiral.length; i++){
-        for (var j = 0 ; j < ss._shell[i].length; j++){
-           // oneEllipse= new THREE.Geometry(); 
-           // oneEllipse.vertices.push(ss._shell[i][j]);  
+   //  for (var i = 80 ; i< ss.spiral.length; i++){
+   //      for (var j = 0 ; j < ss._shell[i].length; j++){
+   //         // oneEllipse= new THREE.Geometry(); 
+   //         // oneEllipse.vertices.push(ss._shell[i][j]);  
 
-          if (j ==0 ){
-              radius = .2;
-            }
-           else if(j%10==0){
-            radius = 0.1 ;//0.3;
-           }else{
-            radius = 0.05; //0.5 //0.1 = one drop 
-           }
-           sphere = new THREE.Mesh( new THREE.SphereGeometry( radius, widthSegments, heightSegments ), material );
-           pos = ss._shell[i][j]; 
+   //        if (j ==0 ){
+   //            radius = .2;
+   //          }
+   //         else if(j%10==0){
+   //          radius = 0.1 ;//0.3;
+   //         }else{
+   //          radius = 0.05; //0.5 //0.1 = one drop 
+   //         }
+   //         sphere = new THREE.Mesh( new THREE.SphereGeometry( radius, widthSegments, heightSegments ), material );
+   //         pos = ss._shell[i][j]; 
 
-            sphere.position.set( pos.x, pos.y, pos.z );
-            scene.add( sphere );
+   //          sphere.position.set( pos.x, pos.y, pos.z );
+   //          scene.add( sphere );
 
 
 
-        }
-    }
+   //      }
+   //  }
+       //---- DRAW IN DOTS 
+
 
     //DRAW PLANES ---
-    var surfaceGeom = new THREE.Geometry(); 
 
-    var tempInd = 118 ;
-    var lastCstep = ss.cSteps-1; 
-    //counter clockwise 
-    for (var i = 0; i < lastCstep ; i++){
-      surfaceGeom.vertices.push( ss._shell[tempInd][i]  )
-      surfaceGeom.vertices.push( ss._shell[tempInd+1][i] )
-      surfaceGeom.vertices.push(  ss._shell[tempInd][i+1])
-      surfaceGeom.faces.push( new THREE.Face3( i*3, i*3+1, i*3+2 ) );
+    var startingIndex = 118 ;//70; //reduce calculation
+   
+    var surfaceMaterial = new THREE.MeshPhongMaterial({side: THREE.DoubleSide,  color: 0xFFFFFF, shading: THREE.FlatShading}); 
+    for (var tempInd = ss.spiral.length-2 ; tempInd >=startingIndex ; tempInd--){
+      
+       var surfaceGeom = new THREE.Geometry(); 
+       // console.log("tempInd ", tempInd);
+      var nexti = 0 ;
+      //counter clockwise triangle band
+      for (var i = 0; i < ss.cSteps ; i++){
+        // D-B
+        // |/|
+        // C-A
+
+        nexti = i+1; 
+
+        if(i == ss.cSteps-1) {nexti = 0 ;}//loop back to first dot in ring
+
+        surfaceGeom.vertices.push( ss._shell[tempInd][i]  )  //A
+        surfaceGeom.vertices.push( ss._shell[tempInd+1][i] ) //B
+        surfaceGeom.vertices.push(  ss._shell[tempInd][nexti]) //C
+        
+        surfaceGeom.faces.push( new THREE.Face3( i*6, i*6+1, i*6+2 ) ); //ABC
+
+        surfaceGeom.vertices.push(  ss._shell[tempInd][nexti]) //C
+        surfaceGeom.vertices.push( ss._shell[tempInd+1][i] ) //B
+        surfaceGeom.vertices.push(  ss._shell[tempInd+1][nexti]) //D
+
+        surfaceGeom.faces.push( new THREE.Face3( i*6+3, i*6+4, i*6+5 ) ); //CBD
+
+      }
+
+       var surfaceMesh = new THREE.Mesh( surfaceGeom, surfaceMaterial );
+        scene.add(surfaceMesh);
+
     }
 
-     surfaceGeom.vertices.push( ss._shell[tempInd][lastCstep]  )
-      surfaceGeom.vertices.push( ss._shell[tempInd+1][lastCstep] )
-      surfaceGeom.vertices.push(  ss._shell[tempInd][0])
-      surfaceGeom.faces.push( new THREE.Face3( lastCstep*3 , lastCstep*3+1, lastCstep*3+2 ) );
+    // //adding end caps 
+    // //ending
+    // var capGeom = new THREE.Geometry(); 
+    // var endInd = ss.spiral.length - 1; 
+    // //center 
+    // var center = ss.spiral[endInd] ; //
+    // // console.log(ellipseCenter)
+    // for(var i = 0 ; i <ss.cSteps; i++){
+    //     capGeom.vertices.push(center);
+    //     capGeom.vertices.push(ss._shell[endInd][(i+1)%ss.cSteps]); //wrap around 
+    //     capGeom.vertices.push(ss._shell[endInd][i]); 
+    //     capGeom.faces.push( new THREE.Face3( i*3, i*3+1, i*3+2 ) ); //ABC
+    // } 
 
-    var surfaceMesh = new THREE.Mesh( surfaceGeom, new THREE.MeshBasicMaterial({color:0x660033,         side:THREE.DoubleSide}));
-    scene.add(surfaceMesh);
+    // var capMesh = new THREE.Mesh( capGeom, surfaceMaterial );
+    // scene.add(capMesh);
 
-    
+    // //starting //REVERSE! 
+    // var bCapGeom = new THREE.Geometry(); 
+    // var bEndInd = startingIndex; 
+    // //center 
+    // var bCenter = ss.spiral[bEndInd] ; //
+    // console.log(bCenter)
+    // for(var i = 0 ; i <ss.cSteps; i++){
+    //     bCapGeom.vertices.push(bCenter);
+    //     bCapGeom.vertices.push(ss._shell[bEndInd][i]); 
+    //     bCapGeom.vertices.push(ss._shell[bEndInd][(i+1)%ss.cSteps]); //wrap around 
+      
+    //     bCapGeom.faces.push( new THREE.Face3( i*3, i*3+1, i*3+2 ) ); //ABC
+    // } 
+
+    // var bCapMesh = new THREE.Mesh( bCapGeom, surfaceMaterial );
+    // scene.add(bCapMesh);
+
+
 
     // ----- DRAW PLANES
 
-    //---- DRAW IN DOTS 
 
 
 
